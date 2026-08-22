902c32c736155b2b7e14362fdb5ac4ab3294e778
ItsLouis30 <luissrv30@gmail.com>
2026-08-22 18:11:36 -0500

    feat: actualizar el tema por defecto a oscuro en el proveedor de temas

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
+          forcedTheme="dark"
           disableTransitionOnChange
         >
           {children}
