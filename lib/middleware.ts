c38a349b0f4795a82bc539c39db9c6949e509388
ItsLouis30 <luissrv30@gmail.com>
2026-08-23 00:17:22 -0500

    feat: actualizar la lógica de redirección en la función updateSession para permitir acceso a dashboard y onboarding

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -40,9 +40,8 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/shared')
+    (request.nextUrl.pathname.startsWith('/dashboard') ||
+      request.nextUrl.pathname.startsWith('/onboarding'))
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
